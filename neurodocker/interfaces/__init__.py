7dddf0f389441b772a49b659c34a796b1fe4af52
Steffen <stebo85@web.de>
2020-05-05 14:52:21 +1000

    maybe key error solution

--- neurodocker/interfaces/__init__.py
+++ neurodocker/interfaces/__init__.py
@@ -1,6 +1,6 @@
 """Module-level interface imports"""
 
 from neurodocker.interfaces.interfaces import (
-    AFNI, AFNI, ANTs, Convert3D, Dcm2niix, FSL, FreeSurfer, MatlabMCR, MINC,
+    AFNI, AFNI, ANTs, Convert3D, Dcm2niix, FSL, FreeSurfer, ITKsnap, MatlabMCR, MINC,
     Miniconda, MRtrix3, NeuroDebian, PETPVC, SPM12
 )
